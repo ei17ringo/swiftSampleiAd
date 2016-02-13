aa4abaaf8c0893d9b29b600a9a1f02ad7efae168
ei17ringo <ei17ringo@gmail.com>
2016-02-13 11:34:05 +0800

    commit init

--- sampleiAd/ViewController.swift
+++ sampleiAd/ViewController.swift
@@ -7,12 +7,17 @@
 //
 
 import UIKit
+import iAd
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myAdBanner: ADBannerView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        self.canDisplayBannerAds = true
+        self.myAdBanner.hidden = true
+
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +25,17 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    func bannerViewDidLoadAd(banner: ADBannerView!) {
+        self.myAdBanner?.hidden = false
+    }
+    
+    func bannerViewActionShouldBegin(banner: ADBannerView!, willLeaveApplication willLeave: Bool) -> Bool {
+        return willLeave
+    }
+    
+    func bannerView(banner: ADBannerView!, didFailToReceiveAdWithError error: NSError!) {
+        self.myAdBanner?.hidden = true
+    }
 
 }
 
